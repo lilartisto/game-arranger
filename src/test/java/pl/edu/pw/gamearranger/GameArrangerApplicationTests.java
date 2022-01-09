7f9799e13c979d5ca95978f0105ed5051323d30a
lilartisto <orginalartis@gmail.com>
2022-01-09 18:12:58 +0100

    project initialization

---
+++ src/test/java/pl/edu/pw/gamearranger/GameArrangerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.gamearranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameArrangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
